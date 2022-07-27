b22cc09f11f1205ef3e658e5dfd6f868a55ae754
Ricardo Mendes <rokusu@gmail.com>
2022-07-27 12:37:04 +0100

    point application adapter to API preview endpoint

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,7 +1,7 @@
 import JSONAPIAdapter from '@ember-data/adapter/json-api';
 import { underscore } from '@ember/string';
 export default class ApplicationAdapter extends JSONAPIAdapter {
-  host = 'http://localhost:3000';
+  host = 'https://api-preview.netrunnerdb.com';
   namespace = 'api/v3/public';
 
   pathForType(type) {
